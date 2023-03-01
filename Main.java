d21d85d0c3bb30f397655a70f1cbbad899b35e1e
Amusseus <amogh.prajapati@gmail.com>
2023-03-01 00:42:30 -0800

    Adding Threads and Panels

--- Main.java
+++ Main.java
@@ -1,17 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
+public class Main extends JFrame{
+    private ChatPanel chatpanel;
+    private Board board;
+    public Main(){
+        super("Whose that Pokemon????");
+        chatpanel = new ChatPanel();
+        board = new Board();
 
-public class Main {
+        // layout
+        setLayout(new GridLayout(0,2));
+        add(chatpanel);
+        add(board);
+    }
 
     public static void main(String[] args){
-        JFrame f = new JFrame();
-        // Add panels here
-        // Also creates threads later
-        //ChatPanel chatPanel = new ChatPanel();
+        // setup
+        Main main = new Main();
+        main.setSize(900,900);
+        main.setVisible(true);
+        main.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        //f.add(chatPanel);
-        f.setSize(900, 400);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //threads
+        Thread chatThread = new Thread();
+        Thread boardThread = new Thread();
+        chatThread.start();
+        boardThread.start();
     }
 
 
